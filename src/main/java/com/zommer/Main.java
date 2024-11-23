42fed12e4fda7065fbdfa36ca8f28d54418c9dd7
Andrey Zommer <zommer.02@mail.ru>
2024-11-23 15:57:22 +0300

    add test for strings and answer for 21 quest

--- src/main/java/com/zommer/Main.java
+++ src/main/java/com/zommer/Main.java
@@ -1,7 +1,14 @@
 package com.zommer;
 
+import com.zommer.core.first_part.ProcedureJava;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ProcedureJava.test(new StringBuffer("")); // StringBuffer: 35117ms.
+            ProcedureJava.test(new StringBuilder("")); // StringBuilder: 3358ms.
+        } catch (java.io.IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
